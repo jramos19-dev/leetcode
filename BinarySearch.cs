b2cd9c533d5638f9937b06267b3c349c8c73649e
jramos19_dev <jlramoss@yahoo.com.mx>
2025-06-08 12:08:05 -0500

    more leetcode in binary search

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -116,6 +116,35 @@ namespace leetcode
 
         }
 
+        public int SearchInsert(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                if (nums[mid] < target)
+                {
+                    low = mid + 1;
+                }
+                if (nums[mid] > target)
+                {
+                    high = mid - 1;
+                }
+
+            }
+
+            return low;
+            
+    }
+
+
 
 
     }
